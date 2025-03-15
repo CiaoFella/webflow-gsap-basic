cced11ad995112c1b4be18711a1a645b40bcf50d
CiaoFella <ciao@julianfella.de>
2025-03-15 15:25:39 +0100

    Enhance webpack configuration to support SCSS files and add development server settings. Updated package.json with new dependencies and scripts for serving the application.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const fs = require("fs");
 
-// Get all JS files from src directory
+// Get all JS and SCSS files from src directory
 const srcDir = path.resolve(__dirname, "src");
 const entries = {};
 
-// Function to recursively find all JS files in src directory
-function findJsFiles(dir) {
+// Function to recursively find all JS and SCSS files in src directory
+function findFiles(dir) {
   const files = fs.readdirSync(dir, { withFileTypes: true });
 
   files.forEach((file) => {
     if (file.isDirectory()) {
-      findJsFiles(path.join(dir, file.name));
+      findFiles(path.join(dir, file.name));
     } else if (file.name.endsWith(".js")) {
       const relativePath = path.relative(srcDir, path.join(dir, file.name));
       const entryName = relativePath.replace(/\.js$/, "");
       entries[entryName] = path.join(dir, file.name);
+    } else if (file.name.endsWith(".scss")) {
+      const relativePath = path.relative(srcDir, path.join(dir, file.name));
+      const entryName = relativePath.replace(/\.scss$/, "");
+
+      // Only add SCSS as entry if there's no JS file with the same name
+      if (!entries[entryName]) {
+        entries[entryName] = path.join(dir, file.name);
+      } else {
+        // If there's already a JS entry, add the SCSS file to it
+        const existingEntry = entries[entryName];
+        entries[entryName] = [existingEntry, path.join(dir, file.name)];
+      }
     }
   });
 }
@@ -27,8 +40,8 @@ if (!fs.existsSync(srcDir)) {
   fs.mkdirSync(srcDir, { recursive: true });
 }
 
-// Find all JS files in src directory
-findJsFiles(srcDir);
+// Find all JS and SCSS files in src directory
+findFiles(srcDir);
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -54,6 +67,22 @@ module.exports = (env, argv) => {
             },
           },
         },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [["autoprefixer"]],
+                },
+              },
+            },
+            "sass-loader",
+          ],
+        },
       ],
     },
     optimization: {
@@ -72,7 +101,20 @@ module.exports = (env, argv) => {
         }),
       ],
     },
-    plugins: [new CleanWebpackPlugin()],
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      compress: true,
+      port: 3456, // Changed port to 3456 as requested
+      hot: true,
+    },
     devtool: false,
     mode: isProduction ? "production" : "development",
   };
